fde6c030f06b74bd582a482c02d1f20320ee12e8
Ajay Kumar <ajay011.sharma@hotmail.com>
2020-08-14 06:27:15 +0530

    first update to first steps

--- demo-1/vars.tf
+++ demo-1/vars.tf
@@ -5,15 +5,16 @@ variable "AWS_SECRET_KEY" {
 }
 
 variable "AWS_REGION" {
-  default = "eu-west-1"
+  default = "ap-south-1"
 }
 
 variable "AMIS" {
   type = map(string)
   default = {
-    us-east-1 = "ami-13be557e"
-    us-west-2 = "ami-06b94666"
-    eu-west-1 = "ami-0d729a60"
+    us-east-1 = "ami-107d3e61"
+    ap-south-1 = "ami-02b5fbc2cb28b77b8"
+    eu-west-1 = "ami-044f36cc778038e81"
   }
 }
 
+# http://cloud-images.ubuntu.com/locator/ec2/ (Ubuntu Image locator)
--- first-steps/instance.tf
+++ first-steps/instance.tf
@@ -1,11 +1,11 @@
 provider "aws" {
-  access_key = "ACCESS_KEY_HERE"
-  secret_key = "SECRET_KEY_HERE"
-  region     = "us-east-1"
+  access_key = "Enter key"
+  secret_key = "Enter key"
+  region     = "ap-south-1"
 }
 
 resource "aws_instance" "example" {
-  ami           = "ami-0d729a60"
+  ami           = "ami-02b5fbc2cb28b77b8"
   instance_type = "t2.micro"
 }
 
