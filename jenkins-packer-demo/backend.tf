86d22f409c194a1bdab5d4ae9fb37025fb2faa94
Ajay Kumar <ajay011.sharma@hotmail.com>
2020-08-23 17:15:17 +0530

    first

--- jenkins-packer-demo/backend.tf
+++ jenkins-packer-demo/backend.tf
@@ -1,7 +1,7 @@
-#terraform {
-#  backend "s3" {
-#    bucket = "terraform-state-86oip8bj"
-#    key    = "terraform.tfstate"
-#    region = "ap-south-1"
-#  }
-#}
+terraform {
+  backend "s3" {
+    bucket = "terraform-state-121212j"
+    key    = "terraform.tfstate"
+    region = "ap-south-1"
+  }
+}
