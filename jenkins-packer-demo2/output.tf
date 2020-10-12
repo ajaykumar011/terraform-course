08b98b2b158a60012697166525b9f4eea12f653b
Ajay Kumar <ajay011.sharma@hotmail.com>
2020-10-12 06:06:28 +0530

    updating packer-within

--- jenkins-packer-demo2/output.tf
+++
@@ -1,7 +0,0 @@
-output "app-ip" {
-  value = [aws_instance.app-instance.*.public_ip]
-}
-
-output "s3-bucket" {
-  value = aws_s3_bucket.terraform-state.bucket
-}
--- jenkins-packer-demo2/scripts/jenkins-run-terraform.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -ex
-AWS_REGION="ap-south-1"
-cd jenkins-packer-demo2
-S3_BUCKET="clodzone99"
-aws s3 cp s3://${S3_BUCKET}/amivar.tf amivar.tf --region $AWS_REGION
-terraform init
-terraform apply -auto-approve -var APP_INSTANCE_COUNT=1 -target aws_instance.app-instance
